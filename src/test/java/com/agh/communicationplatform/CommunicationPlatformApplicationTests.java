99363fb9efddb398d25acf79a05ce76434bd7845
Dawid Karolewski <dkarolewski@student.agh.edu.com>
2022-03-13 21:49:30 +0100

    Init project

---
+++ src/test/java/com/agh/communicationplatform/CommunicationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.communicationplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
