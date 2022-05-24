b96bb6e83a6e8fc875dc0aee5952376ac2113157
Dawid Karolewski <dawid.karolewski97@gmail.com>
2022-05-24 14:21:35 +0200

    Clean up react project

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import Header from "./components/Header";
+import fetchMeasurements from "./api/api";
+
 
 function App() {
+
+  const [measurements, setMeasurements] = useState([]);
+
+  useEffect(() => {
+    const token = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqb2huLnNtaXRoQGVtYWlsLmNvbSIsImlhdCI6MTY1MjY0NDU5NCwiZXhwIjoxNjUyNzMwOTk0fQ.c6MMmblUopL_8Iq60uOMUz0CYONqVqwH7taTcoxnVFiYmjY5wu4lw37nc-PmEiE_Ky8TFj6JEdQDh4gMkBIYxw'
+    fetchMeasurements(token).then(data => setMeasurements(data))
+  }, [measurements]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <Header/>
+        <p style={{margin: '20px'}}>
+          Measurements: <br/>
+          {JSON.stringify(measurements)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
