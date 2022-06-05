8a6c0465bcbb574425cd44689329f6ac22c81bc0
Dawid Karolewski <dawid.karolewski97@gmail.com>
2022-06-05 17:04:17 +0200

    Add report content

--- src/main/java/com/agh/communicationplatform/report/ReportGenerator.java
+++ src/main/java/com/agh/communicationplatform/report/ReportGenerator.java
@@ -2,6 +2,8 @@ package com.agh.communicationplatform.report;
 
 import com.agh.communicationplatform.audit.AuditEventService;
 import com.agh.communicationplatform.audit.AuditEventType;
+import com.agh.communicationplatform.device.Device;
+import com.agh.communicationplatform.device.DeviceService;
 import org.springframework.stereotype.Component;
 
 import com.lowagie.text.*;
@@ -9,14 +11,17 @@ import com.lowagie.text.pdf.PdfWriter;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class ReportGenerator {
 
     private final AuditEventService auditEventService;
+    private final DeviceService deviceService;
 
-    public ReportGenerator(AuditEventService auditEventService) {
+    public ReportGenerator(AuditEventService auditEventService, DeviceService deviceService) {
         this.auditEventService = auditEventService;
+        this.deviceService = deviceService;
     }
 
     public void export(HttpServletResponse response) throws DocumentException, IOException {
@@ -33,9 +38,15 @@ public class ReportGenerator {
         Font fontParagraph = FontFactory.getFont(FontFactory.HELVETICA);
         fontParagraph.setSize(12);
 
+        List<Device> devices = deviceService.getAllDevices();
         StringBuilder builder = new StringBuilder();
-
-        builder.append("Registered devices:");
+        builder.append("Registered devices:\n\n");
+        devices.forEach(d -> {
+            builder.append("Name: ").append(d.getName()).append("\n");
+            builder.append("Model: ").append(d.getModel()).append("\n");
+            builder.append("State: ").append(d.getState()).append("\n");
+            builder.append("Measurement frequency: ").append(d.getMeasurementFrequency()).append("\n\n");
+        });
 
         Paragraph paragraph2 = new Paragraph(builder.toString(), fontParagraph);
         paragraph2.setAlignment(Paragraph.ALIGN_LEFT);
